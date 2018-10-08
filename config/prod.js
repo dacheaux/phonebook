f369c1b198d18955150bfc63b46cc299fb187eb7
dacheaux <damjanspace@gmail.com>
2018-10-08 19:39:19 +0200

    tweaks and heroku

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,6 @@
 module.exports = {
   host: process.env.DATABASE_HOST,
-<<<<<<< HEAD
   name: process.env.DATABASE_NAME,
-=======
->>>>>>> 7f65c102089f19df2ecf2583e7571a1f5674433c
   user: process.env.USER,
   password: process.env.PASSWORD
 };
